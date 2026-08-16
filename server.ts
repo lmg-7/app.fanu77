8466b669eb9874c7d68170a1c3db3ade26a40295
lmg-7 <lmg72012@gmail.com>
2026-08-16 18:43:09 +0300

    Add files via upload

--- server.ts
+++ server.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
   // Body parser for JSON
   app.use(express.json());
